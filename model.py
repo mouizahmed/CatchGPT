fef27ffdbcad2522d013ffd11b97a3cb3b799370
mouiz ahmed <mouiza@my.yorku.ca>
2023-02-27 02:12:47 -0500

    changing parameters to detect which sentences were ai generated (in mixed result)

--- model.py
+++ model.py
@@ -1,8 +1,10 @@
 import re
 import torch
 import numpy as np
-from transformers import GPT2LMHeadModel, GPT2TokenizerFast
+from transformers import GPT2LMHeadModel, GPT2TokenizerFast, pipeline, set_seed, AutoModel, AutoTokenizer
+from collections import Counter
 from tqdm import tqdm
+import nltk
 
 # print(torch.cuda.is_available())
 # print(torch.zeros(1).cuda())
@@ -19,36 +21,54 @@ class model:
 
     def __call__(self, text):
         lines = re.split('\.[ | \n]', text)
+
         linesFormatted = []
+        allInfo = []
         perplexity_per_line = []
+
         for i, line in enumerate(lines):
+
             if i < len(lines)-1:
                 line = line + "."
+            if len(line) == 0:
+                lines.remove(line)
+                continue
             if line[0] == "\n":
                 line = line[1:]
-            print(line)
-            linesFormatted.append(line)
-            pps = self.getPerplexityPerLine(line).item()
-            perplexity_per_line.append(pps)
-            print(pps)
 
-        print(perplexity_per_line)
+            # self.burstiness(line)
+            # self.generate(line)
+            #self.BPL(line)
+            linesFormatted.append(line)
+            ppl_object = dict()
+            ppl_object["line"] = line
+            pps = self.getPerplexityPerLine(line).item()
+            ppl_object["ppl"] = pps
+            ppl_object["length"] = len(line.split())
+            allInfo.append(ppl_object)
+            perplexity_per_line.append(pps)
 
         data = np.array(perplexity_per_line)
+        #print(data)
         burstiness = np.std(data)
         avgPP = sum(perplexity_per_line) / len(perplexity_per_line)
-        print("Avg Perplexity", avgPP)
-        print("Sample Burstiness", burstiness)
+        
         output = dict()
-        output['avgPP'] = str(round(avgPP, 2))
-        output['burstiness'] = str(round(burstiness, 2))
+        # output['avgPP'] = str(round(avgPP, 2))
+        output['avgPP'] = avgPP
+        output['avgLength'] = sum(item['length'] for item in allInfo ) / len(allInfo)
+        senLengths = np.array(list(item['length'] for item in allInfo))
+        
+        #print(item['length'] for item in allInfo)
+        output['lengthVariation'] = np.std(senLengths)
+        output['burstiness'] = burstiness
         output['PPL'] = perplexity_per_line
         output['lines'] = linesFormatted
         output['maxPPL'] = int(max(perplexity_per_line))
-        output['maxPPL_index'] = perplexity_per_line.index(max(perplexity_per_line))
+        output['maxPPL_index'] = perplexity_per_line.index(
+            max(perplexity_per_line))
         output['maxPPL_line'] = linesFormatted[output['maxPPL_index']]
-        #print(lines)
-        print(output['maxPPL_line'])
+        output['allInfo'] = allInfo
         return output
 
     def getPerplexityPerLine(self, line):
@@ -60,13 +80,14 @@ class model:
         for begin_loc in tqdm(range(0, seq_len, self.stride)):
             end_loc = min(begin_loc + self.max_length, seq_len)
             trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
-            input_ids = encodings.input_ids[:, begin_loc:end_loc].to(self.device)
+            input_ids = encodings.input_ids[:,
+                                            begin_loc:end_loc].to(self.device)
             target_ids = input_ids.clone()
             target_ids[:, :-trg_len] = -100
 
             with torch.no_grad():
                 outputs = self.model(input_ids, labels=target_ids)
-
+                #print(outputs)
                 # loss is calculated using CrossEntropyLoss which averages over input tokens.
                 # Multiply it with trg_len to get the summation instead of average.
                 # We will take average over all the tokens to get the true average
@@ -80,11 +101,10 @@ class model:
                 break
 
         ppl = torch.exp(torch.stack(nlls).sum() / end_loc)
+        #print(ppl.item() / seq_len)
         return ppl
     
 
-
-
-
+    
 # testModel = model()
 # testModel("Flask is a popular open-source micro web framework for building web applications in Python. It was developed by Armin Ronacher and released in 2010. Flask is designed to be lightweight and flexible, making it easy to get started with and to customize to your needs. It is used by many developers to create simple to complex web applications and APIs. Flask is built on top of the Werkzeug toolkit, which provides low-level utilities for handling HTTP requests and responses, and the Jinja2 templating engine, which allows you to easily create dynamic HTML pages. Flask also provides a number of extensions that you can use to add additional functionality to your application, such as authentication, database integration, and more. One of the key benefits of Flask is its simplicity and minimalism. Flask has a small core that provides basic features, such as routing and request handling, but leaves the rest of the functionality up to you. This allows you to build your application in a modular way, adding only the features you need and keeping your codebase small and maintainable. Flask is often compared to other popular web frameworks such as Django, Pyramid, and Bottle. While these frameworks provide more out-of-the-box features than Flask, they can also be more complex and require more setup time. Flask is a good choice if you want a lightweight and flexible framework that you can customize to your needs.")
--- static/main.js
+++ static/main.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     
     $('form').on('submit', function(event) {
         event.preventDefault();
+    
         //$('#result').hide(); -- average
         $('.answer').hide();
         $('.spinner-border').show();
@@ -16,10 +17,64 @@ $(document).ready(function() {
             $('.spinner-border').hide();
             //$('#result').show(); -- average
             $('.answer').show();
-            $('#avgPP').html(data.avgPP);
-            $('#burstiness').html(data.burstiness);
+            // $('.answer').css('display', 'block');
+            $('#avgPP').html(data.avgPP.toFixed(2));
+            $('#burstiness').html(data.burstiness.toFixed(2));
             $('#maxPPL_line').html(data.maxPPL_line);
             $('#maxPPL').html(data.maxPPL);
+            //$('#lines').html(data.lines);
+            $('#verdict').empty();
+            console.log(data.avgPP < data.burstiness);
+            
+            if (data.avgPP > 80 && (data.avgPP >= data.burstiness)) {
+                var verdict = $("<h4>Your text is most likely written entirely by a human.<h4>");
+                $('#verdict').append(verdict);
+            } else if (data.avgPP > 50 && data.avgPP < 80) {
+                var verdict = $("<h4 class='verdict'>Your text may include parts written by an AI<h4>");
+                $('#verdict').append(verdict);
+            } else if (data.avgPP <= data.burstiness) {
+                var verdict = $("<h4 class='verdict'>Your text may include parts written by an AI<h4>");
+                $('#verdict').append(verdict);
+            } else {
+                var verdict = $("<h4>Your text is most likely written entirely by an AI<h4>");
+                $('#verdict').append(verdict);
+            }
+
+            var list = $("<ul class='ul-style'></ul>");
+            $.each(data.allInfo, function(i, object) {
+                if (data.avgPP > 50 && data.avgPP < 80) { // TO HIGHLIGHT IF PART AI
+      
+                    if (object.ppl < 35 && object.length > data.avgLength + data.lengthVariation) {
+                        var line = $("<li class='li-style-highlight'></li>").text(object.line);
+                        list.append(line);
+                     } else if (object.ppl < 20 && object.length < data.avgLength - data.lengthVariation) {
+                         var line = $("<li class='li-style-highlight'></li>").text(object.line);
+                         list.append(line);
+                     } else if (data.avgPP - object.ppl < 2 * object.burstiness) {
+                            var line = $("<li class='li-style-highlight'></li>").text(object.line);
+                         list.append(line);
+                     } else if (object.ppl < 50 && object.length > 15) {
+                        var line = $("<li class='li-style-highlight'></li>").text(object.line);
+                         list.append(line);
+                     } else if (object.ppl < 35 && object.length < 15) {
+                        var line = $("<li class='li-style-highlight'></li>").text(object.line);
+                         list.append(line);
+                     }
+                    else {
+                        var line = $("<li class='li-style'></li>").text(object.line);
+                        list.append(line);
+                    }
+                } else {
+                    var line = $("<li class='li-style'></li>").text(object.line);
+                    list.append(line);
+                }
+            
+            })
+            $('#sentences').empty();
+            $('#sentences').append(list);
+
+            
+            
         })
 
         
