fa75e135e4ebaadcae0f1c5f3ae3e1a5704dddbc
mouiz ahmed <mouiza@my.yorku.ca>
2023-02-27 20:00:10 -0500

    updated flask site

--- static/main.js
+++ static/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    //console.log("dgsdg")    
+        
     
     $('form').on('submit', function(event) {
         event.preventDefault();
-    
+        $('#submit-button').prop('disabled', true);
         //$('#result').hide(); -- average
         $('.answer').hide();
         $('.spinner-border').show();
@@ -13,7 +13,11 @@ $(document).ready(function() {
             type: 'POST',
             url: '/calculate'
         }).done(function(data) {
-            console.log(data);
+            setTimeout(function() {
+                $('#submit-button').prop('disabled', false);
+              }, 1000);
+            //console.log(data);
+            
             $('.spinner-border').hide();
             //$('#result').show(); -- average
             $('.answer').show();
@@ -24,7 +28,7 @@ $(document).ready(function() {
             $('#maxPPL').html(data.maxPPL);
             //$('#lines').html(data.lines);
             $('#verdict').empty();
-            console.log(data.avgPP < data.burstiness);
+            //console.log(data.avgPP < data.burstiness);
             
             if (data.avgPP > 80 && (data.avgPP >= data.burstiness)) {
                 var verdict = $("<h4>Your text is most likely written entirely by a human.<h4>");
@@ -44,23 +48,14 @@ $(document).ready(function() {
             $.each(data.allInfo, function(i, object) {
                 if (data.avgPP > 50 && data.avgPP < 80) { // TO HIGHLIGHT IF PART AI
       
-                    if (object.ppl < 35 && object.length > data.avgLength + data.lengthVariation) {
-                        var line = $("<li class='li-style-highlight'></li>").text(object.line);
-                        list.append(line);
-                     } else if (object.ppl < 20 && object.length < data.avgLength - data.lengthVariation) {
-                         var line = $("<li class='li-style-highlight'></li>").text(object.line);
-                         list.append(line);
-                     } else if (data.avgPP - object.ppl < 2 * object.burstiness) {
-                            var line = $("<li class='li-style-highlight'></li>").text(object.line);
-                         list.append(line);
-                     } else if (object.ppl < 50 && object.length > 15) {
+            
+                    if (object.ppl < data.avgPP + 0.5 * data.burstiness && object.length > 15) {
                         var line = $("<li class='li-style-highlight'></li>").text(object.line);
                          list.append(line);
-                     } else if (object.ppl < 35 && object.length < 15) {
+                     } else if (object.ppl < data.avgPP - 0.5 * data.burstiness && object.length < 15) {
                         var line = $("<li class='li-style-highlight'></li>").text(object.line);
                          list.append(line);
-                     }
-                    else {
+                     } else {
                         var line = $("<li class='li-style'></li>").text(object.line);
                         list.append(line);
                     }
@@ -72,8 +67,88 @@ $(document).ready(function() {
             })
             $('#sentences').empty();
             $('#sentences').append(list);
-
+           
+             $('#myChart').remove();
+             $('#myChart2').remove();
+            $('#chart-container1').append('<canvas id="myChart" responsive="true"></canvas>');
+            $('#chart-container2').append('<canvas id="myChart2" responsive="true"></canvas>');
+            const ctx = document.getElementById('myChart');
+            const ctx2 = document.getElementById('myChart2');
             
+
+            new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: data.allInfo.map(function(object) {
+                        return object.line;
+                      }),
+                    datasets: [{
+                        label: 'PPL',
+                        data: data.allInfo.map(function(object) {
+                            return object.ppl;
+                          }),
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            
+                        },
+                        x: {
+                            ticks: {
+                                callback: function(label) {
+                                  if (label.length > 10) { 
+                                    //console.log(label.length);
+                                    return label.substring(0, 10) + '...';
+                                  } else {
+                                    return label;
+                                  }
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+
+            new Chart(ctx2, {
+                type: 'line',
+                data: {
+                    labels: data.allInfo.map(function(object) {
+                        return object.line;
+                      }),
+                    datasets: [{
+                        label: 'Sentence Length',
+                        data: data.allInfo.map(function(object) {
+                            return object.length;
+                          }),
+                        borderWidth: 1,
+                        borderColor: 'rgb(255, 0, 0)',
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            
+                        },
+                        x: {
+                            ticks: {
+                                callback: function(label) {
+                                  if (label.length > 10) { 
+                                    return label.substring(0, 10) + '...';
+                                  } else {
+                                    return label;
+                                  }
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+
+           
             
         })
 
